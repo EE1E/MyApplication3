6bab92640dd57e07d1a6e5bed71f15f554c06df8
axk308 <in06khattab@gmail.com>
2014-06-04 13:40:53 +0100

    Added gps

--- app/src/main/java/com/example/myapplication3/app/FullscreenActivity.java
+++ app/src/main/java/com/example/myapplication3/app/FullscreenActivity.java
@@ -21,7 +21,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import java.util.Date;
-
+import android.widget.Toast;
 import java.text.DateFormat;
 
 
@@ -31,7 +31,7 @@ import java.text.DateFormat;
  *
  * @see SystemUiHider
  */
-public class FullscreenActivity extends Activity implements LocationListener {
+public class FullscreenActivity extends Activity{
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -60,38 +60,14 @@ public class FullscreenActivity extends Activity implements LocationListener {
      */
     private TextView txt1;
 
-    // flag for GPS status
-    boolean isGPSEnabled = false;
 
-    // flag for network status
-    boolean isNetworkEnabled = false;
+    // GPSTracker class
+    GPS gps;
+    Button btn1;
 
-    // flag for GPS status
-    boolean canGetLocation = false;
 
-    Location location; // location
-    double latitude; // latitude
-    double longitude; // longitude
+    public void btn1clk(View arg0) {
 
-    FullscreenActivity gps;
-
-    // The minimum distance to change Updates in meters
-    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1; // 1 meters
-
-    // The minimum time between updates in milliseconds
-    private static final long MIN_TIME_BW_UPDATES = 100 * 60 * 1; // 6 seconds
-
-    // Declaring a Location Manager
-    protected LocationManager locationManager;
-    private final Context mContext;
-
-    public FullscreenActivity(Context mContext) {
-        this.mContext = mContext;
-        getLocation();
-    }
-
-    public void btn1clk(View v) {
-        Button btn1 = (Button) v;
 
 
         txt1 = (TextView) findViewById(R.id.txt1);
@@ -100,8 +76,8 @@ public class FullscreenActivity extends Activity implements LocationListener {
         txt1.append("\n" + currentDateTimeString);
 
 
-
-        // check if GPS enabled
+        gps = new GPS(FullscreenActivity.this);
+        //check if GPS enabled
         if(gps.canGetLocation()){
 
             txt1.append("\n" + gps.getLatitude()); // returns latitude
@@ -119,118 +95,6 @@ public class FullscreenActivity extends Activity implements LocationListener {
 
     }
 
-    public Location getLocation() {
-        try {
-            locationManager = (LocationManager) mContext
-                    .getSystemService(LOCATION_SERVICE);
-
-            // getting GPS status
-            isGPSEnabled = locationManager
-                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
-
-            // getting network status
-            isNetworkEnabled = locationManager
-                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-
-            if (!isGPSEnabled && !isNetworkEnabled) {
-                // no network provider is enabled
-            } else {
-                this.canGetLocation = true;
-                // First get location from Network Provider
-                if (isNetworkEnabled) {
-                    locationManager.requestLocationUpdates(
-                            LocationManager.NETWORK_PROVIDER,
-                            MIN_TIME_BW_UPDATES,
-                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-                    Log.d("Network", "Network");
-                    if (locationManager != null) {
-                        location = locationManager
-                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                        if (location != null) {
-                            latitude = location.getLatitude();
-                            longitude = location.getLongitude();
-                        }
-                    }
-                }
-                // if GPS Enabled get lat/long using GPS Services
-                if (isGPSEnabled) {
-                    if (location == null) {
-                        locationManager.requestLocationUpdates(
-                                LocationManager.GPS_PROVIDER,
-                                MIN_TIME_BW_UPDATES,
-                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-                        Log.d("GPS Enabled", "GPS Enabled");
-                        if (locationManager != null) {
-                            location = locationManager
-                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                            if (location != null) {
-                                latitude = location.getLatitude();
-                                longitude = location.getLongitude();
-                            }
-                        }
-                    }
-                }
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return location;
-    }
-
-    public double getLatitude(){
-        if(location != null){
-            latitude = location.getLatitude();
-        }
-
-        // return latitude
-        return latitude;
-    }
-
-    public double getLongitude(){
-        if(location != null){
-            longitude = location.getLongitude();
-        }
-
-        // return longitude
-        return longitude;
-    }
-
-    public boolean canGetLocation() {
-        return this.canGetLocation;
-    }
-
-
-
-    public void showSettingsAlert(){
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
-
-        // Setting Dialog Title
-        alertDialog.setTitle("GPS is settings");
-
-        // Setting Dialog Message
-        alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
-
-        // On pressing Settings button
-        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog,int which) {
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                mContext.startActivity(intent);
-            }
-        });
-
-        // on pressing cancel button
-        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-
-        // Showing Alert Message
-        alertDialog.show();
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -239,28 +103,9 @@ public class FullscreenActivity extends Activity implements LocationListener {
 
 
         txt1 = (TextView) findViewById(R.id.txt1);
-
+        btn1 = (Button) findViewById(R.id.btn1);
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
-    }
 
 
 
